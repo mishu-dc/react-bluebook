cd0636a250ceb74923d6224e9eb927fd50c6df62
Faisal <fahmed@drfirst.com>
2018-06-08 14:56:42 -0400

    Added react-router-bootstrap
    Used LinkContainer in Navigation Component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Navbar, Nav, NavItem, MenuItem, NavDropdown} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+import { IndexLinkContainer } from 'react-router-bootstrap';
 
 class Navigation extends Component{
     render(){
@@ -8,30 +10,30 @@ class Navigation extends Component{
                 <Navbar inverse collapseOnSelect>
                     <Navbar.Header>
                         <Navbar.Brand>
-                            <a href="/">React-BlueBook</a>
+                            <a href="/">React-Blue Book</a>
                         </Navbar.Brand>
                         <Navbar.Toggle />
                     </Navbar.Header>
                     <Navbar.Collapse>
                         <Nav>
-                            <NavItem eventKey={1} href="/">
-                                Home
-                            </NavItem>
+                            <LinkContainer to="Home" href="/">
+                                <NavItem eventKey={1}>Home</NavItem>
+                            </LinkContainer>
                             <NavDropdown eventKey={3} title="Basic" id="basic-nav-dropdown">
-                                <MenuItem href="/Brand" eventKey={3.1}>Brand</MenuItem>
-                                <MenuItem href="/Product" eventKey={3.2}>Product</MenuItem>
+                                <LinkContainer to="Brand" href="/Brand"><MenuItem eventKey={3.1}>Brand</MenuItem></LinkContainer>
+                                <LinkContainer to="Product" href="/Product"><MenuItem eventKey={3.2}>Product</MenuItem></LinkContainer>
                                 <MenuItem divider />
-                                <MenuItem href="/Distributor" eventKey={3.3}>Distributor</MenuItem>
-                                <MenuItem href="/Market" eventKey={3.4}>Market</MenuItem>
+                                <LinkContainer to="Distributor" href="/Distributor"><MenuItem eventKey={3.3}>Distributor</MenuItem></LinkContainer>
+                                <LinkContainer to="Market" href="/Market"><MenuItem eventKey={3.4}>Market</MenuItem></LinkContainer>
                                 <MenuItem divider />
-                                <MenuItem href="/FieldForce" eventKey={3.5}>Field Force</MenuItem>
+                                <LinkContainer to="FieldForce" href="/FieldForce"><MenuItem eventKey={3.5}>Field Force</MenuItem></LinkContainer>
                             </NavDropdown>
-                            <NavItem eventKey={4} href="/About">
-                                About
-                            </NavItem>
-                            <NavItem eventKey={5} href="/Contact">
-                                Contact
-                            </NavItem>
+                            <LinkContainer to="About" href="/About">
+                                <NavItem eventKey={2}>About</NavItem>
+                            </LinkContainer>
+                            <LinkContainer to="Contact" href="/Contact">
+                                <NavItem eventKey={4}>Contact</NavItem>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
