7240e1cef3274cc9022190103c472a08eb03ffd1
faisal ahmed <mishu_dc@yahoo.com>
2018-07-30 01:21:42 -0400

    Added Login Component

---
+++ src/components/LogIn.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+import {Form, Button, FormGroup, FormControl, ControlLabel, Checkbox} from 'react-bootstrap';
+
+class LogIn extends Component{
+
+    constructor(){
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        const email = event.target.elements.email.value;
+        const password = event.target.elements.password.value;
+        console.log(this.props);
+        this.props.verifyLogin({"userName": email, "password": password});
+
+    }
+    
+    render(){
+        return (
+            <Grid>
+                <Row className="show-grid">
+                    <Form horizontal onSubmit={this.handleSubmit}>
+                        <FormGroup>
+                            <Col smOffset={2} sm={5}>
+                                <Col componentClass={ControlLabel} sm={10}> {this.props.user.message} </Col>
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Col componentClass={ControlLabel} sm={2}> Email </Col>
+                            <Col sm={5}> <FormControl type="email" placeholder="Email" id="email" name="email"/> </Col>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Col componentClass={ControlLabel} sm={2}> Password </Col>
+                            <Col sm={5}> <FormControl type="password" placeholder="Password" id="password" name="password" /> </Col>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Col smOffset={2} sm={5}>
+                                <Checkbox id="rememberMe" name="rememberMe">Remember me</Checkbox>
+                            </Col>
+                        </FormGroup>
+                        
+                        <FormGroup>
+                            <Col smOffset={2} sm={5}>
+                                <Button type="submit">Sign in</Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </Row>
+            </Grid>
+        );
+    }
+}
+
+export default LogIn;
--- src/index.js
+++ src/index.js
@@ -9,31 +9,15 @@ import thunkMiddleware from 'redux-thunk';
 
 import App from './components/App';
 
-import { addBrand, fetchBrands } from './actions/brandActions';
-import { addProduct, fetchProducts } from './actions/productActions';
-import { addDistributor, fetchDistributors } from './actions/distributorActions';
-import { addFieldforce, fetchFieldforces } from './actions/fieldforceActions';
-import { addMarket, fetchMarkets } from './actions/marketActions';
-import { verifyLogin } from './actions/userActions';
+import { verifyLogin, userLogout } from './actions/userActions';
 
 import  { rootReducer }  from "./reducers/index";
 
 const store = createStore(rootReducer,applyMiddleware(thunkMiddleware));
 
 window.store = store;
-window.addBrand = addBrand;
-window.addProduct = addProduct;
-window.fetchBrands = fetchBrands;
-window.fetchProducts = fetchProducts;
 window.verifyLogin = verifyLogin;
-window.fetchDistributors = fetchDistributors;
-window.fetchFieldforces = fetchFieldforces;
-window.fetchMarkets = fetchMarkets;
-window.addDistributor = addDistributor;
-window.addFieldforce = addFieldforce;
-window.addMarket = addMarket;
-
-
+window.userLogout = userLogout;
 
 store.subscribe(()=>console.log("store updated", store.getState()));
 
